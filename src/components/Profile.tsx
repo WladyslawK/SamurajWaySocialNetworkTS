782c7021325b3ae55936b33adc36b4134b207e0e
WladyslawK <vladkurr7890@gmail.com>
2022-08-20 08:54:22 +0200

    folder components with components Heade Navbar Profile

---
+++ src/components/Profile.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <>
+            <div className="content">
+                <img src="https://cdn.pixabay.com/photo/2018/08/14/13/23/ocean-3605547__340.jpg" alt="img"/>
+                <div>
+                    ava + description
+                </div>
+                <div>
+                    My Posts
+                    <div>
+                        New post
+                    </div>
+                </div>
+                <div>
+                    Post 1
+                </div>
+                <div>
+                    Post 2
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Profile;
