c566454bfe002b9e86b2637545a20c78ac828985
WladyslawK <vladkurr7890@gmail.com>
2022-10-15 14:45:58 +0200

    Side effects

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {UsersPageType, UsersType} from "../../Redux/usersReducer";
 import style from "./Users.module.css"
 import axios from "axios";
@@ -12,49 +12,22 @@ type UsersComponentType = {
 
 export const Users: React.FC<UsersComponentType> = ({usersInfo, followUser, unfollowUser, setUsersState}) => {
 
+    /*useEffect(()=> {
 
-    const users = [
-        {
-            userId: 1,
-            followed: true,
-            userFirstName: "Andrew",
-            userSecondName: "Woronin",
-            status: "Front End Developer",
-            location: {city: "Warsaw", country: "Poland"}
-        },
-        {
-            userId: 2,
-            followed: false,
-            userFirstName: "Jan",
-            userSecondName: "Kowalski",
-            status: "Front End Developer",
-            location: {city: "Krakow", country: "Poland"}
-        },
-        {
-            userId: 3,
-            followed: true,
-            userFirstName: "Aleksandr",
-            userSecondName: "Szlak",
-            status: "Front End Developer",
-            location: {city: "Verona", country: "Italy"}
-        },
-        {
-            userId: 4,
-            followed: false,
-            userFirstName: "Pawel",
-            userSecondName: "Goral",
-            status: "Front End Developer",
-            location: {city: "Porto", country: "Portugal"}
-        },
-    ]
+    }, [])*/
 
-    if(usersInfo.users.length === 0){
-        axios.get("https://social-network.samuraijs.com/api/1.0/users")
-            .then(response => setUsersState(response.data.items))
-        //setUsersState(users)
+    const getUsers = () => {
+        if (usersInfo.users.length === 0) {
+            axios.get("https://social-network.samuraijs.com/api/1.0/users")
+                .then(response => setUsersState(response.data.items))
+            //setUsersState(users)
+        }
     }
+
+
     return (
         <div>
+            <button onClick={getUsers}>Get Users</button>
             {
                 usersInfo && usersInfo.users.map(user => {
                     return (
